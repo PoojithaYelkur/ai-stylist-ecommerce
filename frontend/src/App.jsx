07608e93a7218fb03dd600032e4132a27f28ab2a
Poojitha Yelkur <poojithayelkur2006@gmail.com>
2026-03-13 23:56:40 +0530

    frontend update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,34 @@ import Login from "./pages/Login";
 import Products from "./pages/Products";
 import Profile from "./pages/Profile";
 import Register from "./pages/Register";
-
+import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Login />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/profile" element={<Profile />} />
+
         <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/products"
+          element={
+            <ProtectedRoute>
+              <Products />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
     </BrowserRouter>
   );
